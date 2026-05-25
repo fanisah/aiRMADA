12834916abcd9f2fc7865657825a71ce23e55ac1
RiyanNaffa <riyannaffanusafara@gmail.com>
2026-05-25 15:52:51 +0700

    feat: update mocks and types

---
+++ packages/mocks/src/warehouses.ts
@@ -0,0 +1,40 @@
+import { Warehouse } from '@airmada/types'
+
+export const mockWarehouses: Warehouse[] = [
+    {
+        id: '1',
+        name: 'Warehouse UGM',
+        address: 'Blk. D, Jl. Podocarpus II No.13, Sagan, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281',
+        lat: -7.77379064874644,
+        long: 110.37834523462821,
+        created_at: '2026-05-25 07:45:46.862483+00',
+        updated_at: '2026-05-25 07:45:46.862483+00'
+    },
+    {
+        id: '2',
+        name: 'Warehouse Gamping',
+        address: 'Jl. Patukan, Gamping Tengah, Ambarketawang, Kec. Gamping, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55294',
+        lat: -7.799318873773332,
+        long: 110.324350315422,
+        created_at: '2026-05-25 07:46:46.025395+00',
+        updated_at: '2026-05-25 07:46:46.025395+00'
+    },
+    {
+        id: '3',
+        name: 'Warehouse Mlati',
+        address: 'Jl. Sendari, RT.02/RW.14, Sendon, Tirtoadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55287',
+        lat: -7.735685816848973,
+        long: 110.32861532783177,
+        created_at: '2026-05-25 07:47:38.387472+00',
+        updated_at: '2026-05-25 07:47:38.387472+00'
+    },
+    {
+        id: '4',
+        name: 'Warehouse Berbah',
+        address: 'Jl. Raya Berbah, Wotgaleh, Sendangtirto, Kec. Berbah, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281',
+        lat: -7.804551567709661,
+        long: 110.42864820473761,
+        created_at: '2026-05-25 07:49:14.866749+00',
+        updated_at: '2026-05-25 07:49:14.866749+00'
+    }
+]
---
+++ packages/types/src/warehouses.ts
@@ -0,0 +1,9 @@
+export interface Warehouse {
+  id: string
+  name: string
+  address: string
+  lat: number
+  long: number
+  created_at: string
+  updated_at: string
+}
